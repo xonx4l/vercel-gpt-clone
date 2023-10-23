8216b2e7c2da0c302b19b4246ad7bdf4689645b7
xonx4l <sk797119@gmail.com>
2023-10-23 10:55:23 +0530

    go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
-`use client `
+"use client " ; 
+import  { useChat} from "ai/react";
 export default function Home() {
+     const { messages, input, handleInputChange, handleSubmit } = useChat();
+     return (
+      <div className= "pt-4 pb-32">
+        {messages.map ((message) => (
+          <div key= {message.id} className="border-t  border-black/10">
+            <div className="max-w-3xl mx-auto py-6 flex ">
+              <span className="ml-3">{message.content}</span>
+            </div>
+          </div> 
+        ))}
 
-  }
+        <form className = "fixed insert-x-0 bottom-10" onSubmit={handleSubmit}>
+         <input placeholder="send a message" value={input} onChange={handleInputChange} />
+        </form>
+      </div>
+     );
+
+}
+        
